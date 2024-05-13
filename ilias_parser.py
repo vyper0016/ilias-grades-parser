d20ff07549b855f7505fbfd06c6a078dcf88ea3d
Achraf Hassine <matritf@gmail.com>
2024-05-13 16:12:06 +0200

    .

--- ilias_parser.py
+++ ilias_parser.py
@@ -4,7 +4,6 @@ import mechanicalsoup as ms
 from bs4 import BeautifulSoup
 import json
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
